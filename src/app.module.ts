b3992b42127fd293edfcb9f4e3123e6a13827d8e
fuadashraful <fuadashraful16@gmail.com>
2025-11-13 14:38:05 +0600

    feat: Configure mysql and typeorm db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,30 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DataSource, DataSourceOptions } from 'typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmConfigService } from './database/typeorm-config.service';
+import databaseConfig from './database/config/database.config';
+import appConfig from './config/app.config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [
+        databaseConfig,
+        appConfig,
+      ],
+      envFilePath: ['.env'],
+    }),
+    TypeOrmModule.forRootAsync({
+      useClass: TypeOrmConfigService,
+      dataSourceFactory: async (options: DataSourceOptions) => {
+        return new DataSource(options).initialize();
+      },
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
